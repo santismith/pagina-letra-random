21ff284acbc791e57b3e27ca47ea01969a53635e
santismith <santii.smiith@hotmail.com>
2021-04-10 16:43:37 -0300

    agregando funcionalidades

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 
-let letraAleatoria = 'A';
+
+letraQuery = document.querySelector('.letra');
 
 
-letraRandom = function() {
 
+const letraRandom = function() {
+    let caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    let letra
+    letra =  caracteres.charAt(Math.floor(Math.random()*26))
+    
+    return letra
 }
 
 
-document.getElementById('boton0').addEventListener("click", function() {
-    
-})
+    document.getElementById('boton0').addEventListener("click", function() {
+        letraQuery.textContent += letraRandom()
+    })
+
+
+    document.getElementById('boton1').addEventListener("click",function(){
+        letraQuery.textContent = ''
+
+    })
+
 
